626d656b31871425fc02689138c90fed6eef4b6e
Vladimir Hrechko <grechko_vo@navis.ru>
2025-07-05 12:05:35 +0300

    feat: Add registration window

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import logging as log
 import kivy
 from kivy.app import App
-from kivy.uix.screenmanager import Screen
+from kivy.uix.screenmanager import Screen, ScreenManager, SlideTransition
 from kivy.uix.behaviors import ButtonBehavior
 from kivy.uix.image import Image
+from kivy.lang import Builder
 
 class ImageButton(ButtonBehavior, Image):
     pass
@@ -17,13 +18,35 @@ class LoginScreen(Screen):
         login = self.ids.login_input.text
         password = self.ids.password_input.text
         print(f"Логин: {login}, Пароль: {password}")
+        
+    def go_registration(self):
+        self.manager.transition = SlideTransition(direction='left')
+        self.manager.current = 'registration'
 
     def open_settings(self):
         print("Открыть настройки")
 
-class LoginApp(App):
+class RegistrationScreen(Screen):
+    def try_register(self):
+        login = self.ids.login_input.text
+        password = self.ids.password_input.text
+        email = self.ids.email_input.text
+        print(f"Логин: {login}, Пароль: {password}, email: {email}")
+    
+    def go_back(self):
+        self.manager.transition = SlideTransition(direction='right')
+        self.manager.current = 'login'
+
+class NotesApp(App):
     def build(self):
-        return LoginScreen()
+        
+        Builder.load_file('login.kv')
+        Builder.load_file('registration.kv')
+        
+        sm = ScreenManager()
+        sm.add_widget(LoginScreen(name='login'))
+        sm.add_widget(RegistrationScreen(name='registration'))
+        return sm
 
 if __name__ == "__main__":
     log.basicConfig(
@@ -34,5 +57,5 @@ if __name__ == "__main__":
     
     log.info("Start Kivy Tutors")
     print_kivy_pathes()
-    LoginApp().run()
+    NotesApp().run()
     log.info("End Kivy Tutors")
